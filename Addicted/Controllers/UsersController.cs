88417dc22616fa2143cb9b5ae1fa97c6ef7ba2f3
Petras Viciunas <petrasviciunas@gmail.com>
2021-03-01 01:45:52 +0200

    Add project files.

---
+++ Addicted/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Addicted.Service;
+
+namespace Addicted.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private IUserService userService;
+
+        public UsersController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        [HttpGet]
+        public ActionResult<string> getAllUsers()
+        {
+            return Ok("Hello World!");
+        }
+    }
+}
---
+++ Addicted/Service/UserService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Addicted.Service
+{
+    public interface IUserService
+    {
+
+    }
+    public class UserService : IUserService
+    {
+    }
+}
